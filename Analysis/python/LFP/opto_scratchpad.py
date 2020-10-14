6e02276bd4e2721a8869dc3632ab99b2469fb25a
nkinsky <nkinsky@umich.edu>
2020-10-14 11:25:51 -0400

    old commits

--- Analysis/python/LFP/opto_scratchpad.py
+++ Analysis/python/LFP/opto_scratchpad.py
@@ -2,12 +2,15 @@
 
 import numpy as np
 import Python3.Binary as ob
+import Analysis.python.LFP.preprocess_data as ppd
+
 
 ## Relevant folder/files
-chan_map_file = r'C:\Users\Nat\Documents\UM\Working\Opto\Rat613\2x32MINT_chan_map_good.txt'
-data_folder = r'C:\Users\Nat\Documents\UM\Working\Opto\Rat613\Rat613Day1\Rat613track_2020-08-01_09-21-55'
+chan_map_file = '/data/GitHub/dibalab_ephys/Channel Maps/MINT/2x32MINT_chan_map_good' # nat laptop location r'C:\Users\Nat\Documents\UM\Working\Opto\Rat613\2x32MINT_chan_map_good.txt'
+data_folder = '/data/Working/Opto Project/Rat 613/Rat613Day1/Rat613simtest_2020-08-01_08-47-11/' # nat laptop location r'C:\Users\Nat\Documents\UM\Working\Opto\Rat613\Rat613Day1\Rat613track_2020-08-01_09-21-55'
 ## Load Data!
 chan_map_dict = ob.LoadChannelMapFromText(chan_map_file)
 chans_use = np.hstack((np.arange(0, 8), np.arange(16, 24)))  # Use only shank 1 and shank 3
-channels_use = [chan for chan in np.asarray(chan_map_dict['0']['mapping'])[chans_use]]
-Data, Rate = ob.Load(data_folder, ChannelMap=channels_use, Experiment=2, Recording=1, mode='r')
+# channels_use = [chan for chan in np.asarray(chan_map_dict['0']['mapping'])[chans_use]]  # not used
+Data, Rate = ob.Load(data_folder, ChannelMap=chans_use, Experiment=1, Recording=1, mode='r')
+event_data = ppd.load_binary_events()
