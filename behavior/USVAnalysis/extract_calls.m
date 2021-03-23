6318c57826dad663418ea45756f784ef5a3c62f1
nkinsky <nkinsky@users.noreply.github.com>
2021-03-23 15:57:45 -0400

    add/fix

--- behavior/USVAnalysis/extract_calls.m
+++ behavior/USVAnalysis/extract_calls.m
@@ -7,8 +7,19 @@ function [times, mean_freq] = extract_calls(Calls, keep_all)
 %   Calls: table loaded in from DeepSqueak output .mat file
 %   keep_all (optional): set to true or 1 to keep ALL calls, even those you
 %   have rejected in DeepSqueak. Default = false.
+%
+%   OUTPUTS:
+%   times: an ncalls x 2 array with the start time of each call in column 1
+%   and end time in column 2
+%
+%   mean_freq: an ncalls x 1 array with the mean frequency of each call
+%   matching the times in the times output.
 
-% first filter out all rejected call
+if nargin < 2
+    keep_all = true;
+end
+
+% first filter out all rejected calls
 if keep_all
     accept_bool = Calls.('Accept');
     Calls = Calls(accept_bool, :);
@@ -20,7 +31,7 @@ Box = Calls.('Box');  % grab out the only real data we want in the Box column.
 
 times = [Box(:,1), Box(:,1) + Box(:,3)];  % get start and end time of each call
 
-mean_freq = mean(Box(:, [2, 4]), 1);
+mean_freq = Box(:,2) + Box(:,4)/2;
 
 end
 
